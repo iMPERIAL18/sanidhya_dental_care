87676fc8ff6344e097d46cf0f8c5eb61ed554799
Hemin Patel <patelhemin55@gmail.com>
2021-07-08 22:46:40 +0530

    added insertXray function and inserting case will be done by seprate function created monthly revenue function

--- sql_connector.py
+++ sql_connector.py
@@ -34,43 +34,64 @@ class Patient:
             mycursor.execute("SELECT MAX(patient_id) FROM patients")
             x = mycursor.fetchone()
             self.p_id = x[0] 
-
-            args = (self.p_id,date.today(),self.case)
-            mycursor.callproc("insertCase",args)
-            clinic_db.commit()
+            self.insertCase()
+            
 
         except:
             print("error")
 
+
+    def insertCase(self):
+        args = (self.p_id,date.today(),self.case)
+        mycursor.callproc("insertCase",args)
+        clinic_db.commit()
+
+    
+    def insertXray(self,address):    
+        args = (self.p_id,address,date.today())
+        mycursor.callproc("insertXray",args)
+        clinic_db.commit()
+        # except:
+        #     print("error")
+
+
     @classmethod
     def getPatient(cls,id):
-        mycursor.execute("SELECT name,phoneno,address FROM patients WHERE patient_id = %s",(id,))
-        
+        mycursor.execute(f"SELECT * FROM patients WHERE patient_id = {id}")
+
         return mycursor.fetchone()
 
-# def insertAppointment(id,date):
-#     try:    
-#         args = (id,date)
-#         mycursor.callproc("insertAppointment",args)
-#         clinic_db.commit()
-#     except:
-#         print("error")
+    
+    
+    def insertAppointment(self,date):
+        try:    
+            args = (self.p_id,date)
+            mycursor.callproc("insertAppointment",args)
+            clinic_db.commit()
+        except:
+            print("error")
 
-# def get_yearlyRevenue():
-#     try:    
-#         mycursor.execute('SELECT * FROM yearly_revenue')
-#         result = mycursor.fetchall()
-#         print(result)
-#     except:
-#         print("error")
-
-# def get_monthlyRevenue():
-#     try:    
-#         mycursor.execute('SELECT * FROM monthly_revenue')
-#         result = mycursor.fetchall()
-#         print(result)
-#     except:
-#         print("error")
+    def createInvoice(self,consulting,paid,pending):
+        args = (self.p_id,consulting,paid,pending)
+        mycursor.callproc("createInvoice",args)
+        clinic_db.commit()
+    @classmethod
+    def get_yearlyRevenue(cls):
+        try:    
+            mycursor.execute('SELECT * FROM yearly_revenue')
+            return mycursor.fetchall()
+            
+        except:
+            print("error")
+            
+    @classmethod
+    def get_monthlyRevenue(cls):
+        try:    
+            mycursor.execute('SELECT * FROM monthly_revenue')
+            result = mycursor.fetchall()
+            return result
+        except:
+            print("error")
 
 # def get_dailyRevenue():
 #     try:    
