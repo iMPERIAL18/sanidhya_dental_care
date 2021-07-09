a3c6c7e4d68f5580389032421cd8c48f98e74633
Hemin Patel <patelhemin55@gmail.com>
2021-07-09 21:31:21 +0530

    created GetRevenue class for generating CSV file for daily, monthly, yearly and custom date revenue

--- sql_connector.py
+++ sql_connector.py
@@ -1,5 +1,6 @@
 from datetime import date
 import mysql.connector  
+from pandas import read_sql_query, DataFrame
 
 # connecting database
 try:
@@ -84,20 +85,62 @@ class Patient:
         except:
             print("error")
             
+    
+
+def get_dailyRevenue():
+    try:    
+        mycursor.execute('SELECT * FROM daily_revenue')
+        result = mycursor.fetchall()
+        print(result)
+    except:
+        print("error")
+
+class GetRevenue:
     @classmethod
     def get_monthlyRevenue(cls):
         try:    
-            mycursor.execute('SELECT * FROM monthly_revenue')
-            result = mycursor.fetchall()
-            return result
+            sql_query = read_sql_query("SELECT * FROM monthly_revenue",clinic_db) 
+
+            fileName = "monthlyRevenue" + date.today().strftime("%d%m%Y")               
+
+            df = DataFrame(sql_query)
+            df.to_csv(f"C:/Users/ASUS/Documents/LabVIEW Data/{fileName}.csv", index = False)   # change the path at last
         except:
             print("error")
 
-# def get_dailyRevenue():
-#     try:    
-#         mycursor.execute('SELECT * FROM daily_revenue')
-#         result = mycursor.fetchall()
-#         print(result)
-#     except:
-#         print("error")
+    @classmethod
+    def get_dailyRevenue(cls):
+        try:    
+            sql_query = read_sql_query("SELECT * FROM daily_revenue",clinic_db) 
 
+            fileName = "dailyRevenue" + date.today().strftime("%d%m%Y")               
+
+            df = DataFrame(sql_query)
+            df.to_csv(f"C:/Users/ASUS/Documents/LabVIEW Data/{fileName}.csv", index = False)   # change the path at last
+        except:
+            print("error")
+
+    @classmethod
+    def get_yearlyRevenue(cls):
+        try:    
+            sql_query = read_sql_query("SELECT * FROM yearly_revenue",clinic_db) 
+
+            fileName = "yearlyRevenue" + date.today().strftime("%d%m%Y")               
+
+            df = DataFrame(sql_query)
+            df.to_csv(f"C:/Users/ASUS/Documents/LabVIEW Data/{fileName}.csv", index = False)   # change the path at last
+        except:
+            print("error")
+
+    @classmethod
+    def custom(cls,date1,date2):
+           
+        sql_query = read_sql_query(f'''SELECT i.invoice_id,p.name,i.payment_date,i.payment AS payment_amount FROM invoices i INNER JOIN patients p ON p.patient_id = i.patient_id WHERE i.payment_date BETWEEN {date1} AND {date2}''',clinic_db) 
+
+        fileName = "customRevenue" + date.today().strftime("%d%m%Y")        #date1 + to + date2 + date.today().strftime("%d%m%Y")              
+
+        df = DataFrame(sql_query)
+        df.to_csv(f"C:/Users/ASUS/Documents/LabVIEW Data/{fileName}.csv", index = False)   # change the path at last
+        
+
+        
