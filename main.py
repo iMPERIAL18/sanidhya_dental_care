71f125b27518f020380a5125a057971075abf72d
Hemin Patel <patelhemin55@gmail.com>
2021-07-09 21:27:17 +0530

    added main.ui revenue.ui created global patient variable which will make easy to transfer patient details in the widgets created Revenue class for generating csv file of revenue

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QCalendarWidget, QDialog, QApplication, QFileDialog
 from PyQt5.uic import loadUi
 import sql_connector 
-import csv
+# import csv
+patient = 0
+
+class Main(QDialog):
+    def __init__(self):
+        super(Main,self).__init__() 
+        loadUi("main.ui",self)
+        self.revenue.clicked.connect(self.gotorevenue)
+        self.case_2.clicked.connect(self.gotocase)
+    
+    def gotorevenue(self):
+        widget.setCurrentIndex(8)
+        
+    def gotocase(self):
+        widget.setCurrentIndex(7)
+   
+
 
 class Case(QDialog):
     def __init__(self):
@@ -13,9 +28,9 @@ class Case(QDialog):
         self.new_case.clicked.connect(self.gotoregister)
         self.old_case.clicked.connect(self.gotosearch)
 
-
+     
     def gotosearch(self):
-         
+        
         widget.setCurrentIndex(2)
  
     def gotoregister(self):
@@ -23,8 +38,8 @@ class Case(QDialog):
         widget.setCurrentIndex(1)
 
 class RegisterPatient(QDialog):
+    
 
-    patient = 0
     def __init__(self):
         super(RegisterPatient,self).__init__()
         loadUi("registerPatient.ui", self)
@@ -51,30 +66,27 @@ class RegisterPatient(QDialog):
             history = self.history.toPlainText()
             case = self.N_O.currentText()
             try:
-                # global patientToRegister 
+                global patient 
                 patientToRegister = sql_connector.Patient(name,phoneno,address,history,case)
                 patientToRegister.insertPatient() 
-                RegisterPatient.patient = patientToRegister
-
-                
-                
-              
+                patient = patientToRegister
+  
             except:
                 print("failed to add patient")
             
         
     def xray(self):
-        if RegisterPatient.patient.p_id != 0:
+        if patient.p_id != 0:
             self.name.clear()
             self.address.clear()
             self.history.clear()
             self.phoneno.clear()
-            GoToXray.gotoxray(self,1,RegisterPatient.patient)
+            GoToXray.gotoxray(patient)
         else:
             print("failed to get id")
 class SearchIntoDatabase(QDialog):
     
-    patient = 0
+    
 
     def __init__(self) :
         super(SearchIntoDatabase,self).__init__()
@@ -89,18 +101,21 @@ class SearchIntoDatabase(QDialog):
     def search(self):
         try:
             
-            
+            global patient
             details = sql_connector.Patient.getPatient(int(self.id.text()))
             self.name.setText(details[1]) 
             self.phoneno.setText(str(details[2]))
             self.address.setText(details[3])
             patientToSearch = sql_connector.Patient(details[1],details[2],details[3],details[4],'O')
             patientToSearch.p_id = self.id.text()
+            print(patientToSearch.p_id)
             patientToSearch.insertCase()
-            SearchIntoDatabase.patient = patientToSearch
+            patient = patientToSearch
             
         except:
             print("sserror")
+
+            
 
     def gotocase(self):
         self.id.clear()
@@ -117,38 +132,28 @@ class SearchIntoDatabase(QDialog):
         widget.setCurrentIndex(3)
 
     def xray(self):
-        if SearchIntoDatabase.patient.p_id != 0:
+        if patient.p_id != 0:
             self.id.clear()
             self.name.clear()
             self.phoneno.clear()
             self.address.clear()
-            GoToXray.gotoxray(self,2,SearchIntoDatabase.patient)
+            GoToXray.gotoxray(patient)
         else:
             print("failed to get id")
         
         
 
-class GoToXray(SearchIntoDatabase, RegisterPatient):
-    def gotoxray(self,index,obj):
-        if index == 2:
-            obj = SearchIntoDatabase.patient
-        
-        elif index == 1:
-            obj =RegisterPatient.patient 
-            
-
-        xray = Xray(obj)
-        widget.addWidget(xray)          # index: 4
+class GoToXray():
+    def gotoxray(self):
         widget.setCurrentIndex(4)
-
+        
         
 
 class Xray(QDialog):
 
-    def __init__(self,patient) :
+    def __init__(self) :
         super(Xray,self).__init__()
         self.address = ''
-        self.patient = patient
         loadUi("xray.ui",self)
         self.browseButton.clicked.connect(self.openFileDialog)
         self.addButton.clicked.connect(self.addtoDatabase)
@@ -162,24 +167,18 @@ class Xray(QDialog):
             self.address = img[0][0]
             self.path.setText(self.address)
 
-    
-        
-        
-
-        
-
 
     def addtoDatabase(self):
         try:
-            self.patient.insertXray(self.address)
+            global patient
+            patient.insertXray(self.address)
+            print(patient.p_id)
             print("done")
         except:
             print("error")
 
 
     def gotoappointment(self):
-        appointment = Appointment(self.patient)
-        widget.addWidget(appointment)
         widget.setCurrentIndex(5)
         
 
@@ -187,22 +186,11 @@ class UpdateIntoDatabase(QDialog):
     def __init__(self) :
         super(UpdateIntoDatabase,self).__init__()
         loadUi("update.ui",self)
-        self.updateButton.clicked.connect(self.month)
-
-    def month(self):
-            
-        rows = sql_connector.Patient.get_yearlyRevenue() 
-        fp = open('C:/Users/ASUS/Desktop/m.csv', 'w')
-        myFile = csv.writer(fp)
-        myFile.writerows(rows)
-        fp.close()
-
     
 
 class Appointment(QDialog):
-    def __init__(self,patient):
+    def __init__(self):
         super(Appointment,self).__init__()
-        self.patient = patient 
         loadUi("appointment.ui",self)
         self.calander = self.findChild(QCalendarWidget, "calanderWidget")
         self.addButton.clicked.connect(self.bookAppointment)
@@ -210,30 +198,50 @@ class Appointment(QDialog):
 
     def bookAppointment(self):
         date = self.calander.selectedDate().toPyDate()
-        
-        self.patient.insertAppointment(date)
+        global patient
+        patient.insertAppointment(date)
         print("done")
 
     def gotoinvoice(self):
         # self.calander.setCurrentPage(2021,7)
-        invoice = createInvoice(self.patient)
-        widget.addWidget(invoice)
         widget.setCurrentIndex(6)
         
 class createInvoice(QDialog):
-    def __init__(self,patient) :
+    def __init__(self) :
         super(createInvoice,self).__init__()
-        self.patient = patient
         loadUi("invoice.ui",self)
         self.finishButton.clicked.connect(self.invoice)
         
 
     def invoice(self):
-        
-        self.patient.createInvoice(self.consulting.text(),self.paid.text(),self.pending.text())
+        global patient
+        patient.createInvoice(self.consulting.text(),self.paid.text(),self.pending.text())
         print("done")
         widget.setCurrentIndex(0)
 
+class Revenue(QDialog):
+    def __init__(self) :
+        super(Revenue,self).__init__()
+        loadUi("revenue.ui",self)
+        self.daily.clicked.connect(self.dailyRev)
+        self.monthly.clicked.connect(self.monthlyRev)
+        self.yearly.clicked.connect(self.yearlyRev)
+        self.custom.clicked.connect(self.customRev)
+
+    def dailyRev(self):
+        sql_connector.GetRevenue.get_dailyRevenue()
+        print("generated")
+
+    def monthlyRev(self):
+        sql_connector.GetRevenue.get_monthlyRevenue()
+        print("generated")
+
+    def yearlyRev(self):
+        sql_connector.GetRevenue.get_yearlyRevenue()
+        print("generated")
+
+    def customRev(self):
+        pass
         
 
 
@@ -241,18 +249,27 @@ class createInvoice(QDialog):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    mainwindow = Case()
+    mainwindow = Main()
+    case = Case()
     patient = RegisterPatient() 
     search = SearchIntoDatabase()
     update = UpdateIntoDatabase()
+    invoice = createInvoice()
+    xray = Xray()
+    appointment = Appointment()
     widget = QtWidgets.QStackedWidget()
+    revenue = Revenue()
+
     #stacking Ui's
-    widget.addWidget(mainwindow)    
-    widget.addWidget(patient)
-    widget.addWidget(search)
-    widget.addWidget(update)
-
-
+    widget.addWidget(mainwindow)    # index: 0   
+    widget.addWidget(patient)       # index: 1
+    widget.addWidget(search)        # index: 2
+    widget.addWidget(update)        # index: 3
+    widget.addWidget(xray)          # index: 4
+    widget.addWidget(appointment)   # index: 5
+    widget.addWidget(invoice)       # index: 6
+    widget.addWidget(case)          # index: 7
+    widget.addWidget(revenue)       # index: 8
     widget.setFixedHeight(800)
     widget.setFixedWidth(1200)
     widget.show()
